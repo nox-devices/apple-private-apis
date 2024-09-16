70518da8aa57212dc0fa53ba391dc81192bcdeae
Tae Hagen <tae.hagen@gmail.com>
2024-09-16 19:12:30 -0400

    Update lib.rs

--- omnisette/src/lib.rs
+++ omnisette/src/lib.rs
@@ -19,8 +19,8 @@ pub mod store_services_core;
 #[cfg(feature = "remote-anisette-v3")]
 pub mod remote_anisette_v3;
 
-#[cfg(target_os = "macos")]
-pub mod aos_kit;
+// #[cfg(target_os = "macos")]
+// pub mod aos_kit;
 
 #[cfg(feature = "remote-anisette")]
 pub mod remote_anisette;
